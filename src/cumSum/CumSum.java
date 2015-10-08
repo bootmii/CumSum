04a3c710a577d28aa31d8c8166aa9c076c35cb99
116060088 <116060088@ECH-STU-05-21C.SSFSD.LOCAL>
2015-10-08 08:54:06 -0700

    Add console instructions

--- src/cumSum/CumSum.java
+++ src/cumSum/CumSum.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class CumSum {
 	public static Scanner stdin = new Scanner(System.in);
 	public static int cumSum() { // returns cumulative sum of integers
-		int sum = 1; // since -1 will always be added to t
+		int sum = 1; // -1 will always be added, so init sum to 1
 		int num = -1;
+		System.out.println("Please input a list of numbers followed by -1:");
 		do {
-			num = stdin.nextInt();
-			sum += num;
+			num = stdin.nextInt(); // get number from console
+			sum += num; // and add it to sum
 		} while(num!=-1);
+		return sum;
 	}
 }
